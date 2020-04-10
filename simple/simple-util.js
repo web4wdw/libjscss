270eefa4040809dbcad5fa42a434130feee70d37
wdw <wdw@0770.xyz>
2020-04-10 17:10:07 +0800

    simple

---
+++ simple/simple-util.js
@@ -0,0 +1,6 @@
+
+
+var getUrlPara = function (name, defVal, url) {
+    var val = (new RegExp('(^|\\?|&|#)' + name+ '=([^&#]*)').exec(url || location.href)||[,,undefined===defVal ? '' : defVal])[2];
+    return null == val ? val : decodeURIComponent(val).replace(/['"<>]/g, "");
+};
